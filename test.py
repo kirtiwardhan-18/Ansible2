31554549e7dcb1da86883c2e1131edd0002f213b
Kirtiwardhan Rawal <60699075+kirtiwardhan-18@users.noreply.github.com>
2020-10-11 21:37:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
----
-# An employee record
-name: Martin D'vloper
-job: Developer
-skill: Elite
-employed: True
-foods:
-  - Apple
-  - Orange
-  - Strawberry
-  - Mango
-languages:
-  perl: Elite
-  python: Elite
-  pascal: Lame
-education: |
-  4 GCSEs
-  3 A-Levels
-  BSc in the Internet of Things
-...
+def info:
+  print( Do read teh readme.md files they are most important)
+  print( I just created this file just get an extension of python on my repo, it looked incomplete sorry cant help it)
