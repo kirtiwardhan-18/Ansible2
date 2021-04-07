a0b3fc0bb41d72ae55d3af3dc1004e181f3ffb8b
Kirtiwardhan Rawal <60699075+kirtiwardhan-18@users.noreply.github.com>
2021-04-07 22:12:36 +0530

    Update test.py
    Text Edited

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 def info:
-  print( Do read teh readme.md files they are most important)
+  print( Do read the readme.md file it is most important)
   print( I just created this file just get an extension of python on my repo, it looked incomplete sorry cant help it)
